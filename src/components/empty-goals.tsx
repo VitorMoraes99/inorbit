90ca32db4df4bec5f3dc0b59b9d1a1c118240b78
Vitor Matheus de Moraes <vitor.matheusm@gmail.com>
2025-01-28 16:03:05 -0300

    feat: criação de metas e concluindo as mesmas

--- src/components/empty-goals.tsx
+++ src/components/empty-goals.tsx
@@ -1,25 +1,31 @@
-import logo from '../assets/logo-in-orbit.svg'
-import letsStart from '../assets/lets-start-illustration.svg'
 import { Plus } from 'lucide-react'
+import { DialogTrigger } from '@radix-ui/react-dialog'
+
+import logo from '../assets/logo-in-orbit.svg'
+import rocketLaunchIllustration from '../assets/lets-start-illustration.svg'
 import { Button } from './ui/button'
-import { DialogTrigger } from './ui/dialog'
 
 export function EmptyGoals() {
-    return(
-        <div className="h-screen flex flex-col items-center justify-center gap-8">
-            <img src={logo} alt="in-orbit" className="w-24 h-auto" />
-            <img src={letsStart} alt="" className="w-64 h-auto" />
-            <p className="text-zinc-300 leading-relaxed max-w-md text-center">
-            Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
-            mesmo?
-            </p>
+  return (
+    <main className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
 
-            <DialogTrigger asChild>
-                <Button>
-                    <Plus className="size-4" />
-                    Cadastrar meta
-                </Button>
-            </DialogTrigger>
-        </div>
-    )
+      <img
+        src={rocketLaunchIllustration}
+        alt="Ilustração de uma mulher controlando um lançamento de um foguete através de um controle remoto"
+      />
+
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
+      </p>
+
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className="size-4" />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </main>
+  )
 }
